db9493f8a23bdc9be14716a665995fcc34ac9e94
peidcud <51078365+peidcud@users.noreply.github.com>
2020-07-17 09:45:50 +0800

    Initial commit

---
+++ part1/part1.py
@@ -0,0 +1,77 @@
+import json
+from datetime import datetime
+
+DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
+
+
+def format_temperature(temp):
+    """Takes a temperature and returns it in string format with the degrees and celcius symbols.
+    
+    Args:
+        temp: A string representing a temperature.
+    Returns:
+        A string contain the temperature and 'degrees celcius.'
+    """
+    return f"{temp}{DEGREE_SYBMOL}"
+
+def convert_date(iso_string):
+    """Converts and ISO formatted date into a human readable format.
+    
+    Args:
+        iso_string: An ISO date string..
+    Returns:
+        A date formatted like: Weekday Date Month Year
+    """
+    d = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S%z")
+    return d.strftime('%A %d %B %Y')
+
+
+def convert_f_to_c(temp_in_farenheit):
+    """Converts an temperature from farenheit to celcius
+
+    Args:
+        temp_in_farenheit: integer representing a temperature.
+    Returns:
+        An integer representing a temperature in degrees celcius.
+    """
+    return round((temp_in_farenheit - 32)*(5/9),1)
+
+def calculate_mean(total, num_items):
+    """Calculates the mean.
+    
+    Args:
+        total: integer representing the sum of the numbers.
+        num_items: integer representing the number of items counted.
+    Returns:
+        An integer representing the mean of the numbers.
+    """
+    return (total / num_items)
+
+def process_weather(forecast_file):
+    """Converts raw weather data into meaningful text.
+
+    Args:
+        forecast_file: A string representing the file path to a file
+            containing raw weather data.
+    Returns:
+        A string containing the processed and formatted weather data.
+    """
+    with open("data/forecast_5days_a.json","r") as read_file:
+        data = json.load(read_file)
+    # The overall min temperature, and the date this will occur.
+    for forecast in data["DailyForecasts"]:
+        min_list = [convert_f_to_c(forecast["Temperature"]["Minimum"]["Value"]),convert_date(forecast["Date"])]
+        print(min_list)
+    # The overall max temperature, and the date this will occur. 
+    # The mean minimum temperature. 
+    # The mean maximum temperature. 
+
+
+
+if __name__ == "__main__":
+    print(process_weather("data/forecast_5days_a.json"))
+
+
+
+
+
