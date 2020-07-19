eeb6488e5cb6619b1d704d5257bc33be3bd5e275
peidcud <51078365+peidcud@users.noreply.github.com>
2020-07-19 18:50:06 +0800

    Part 3b finished, final upload

--- part3/part3b.py
+++ part3/part3b.py
@@ -4,6 +4,11 @@ from datetime import datetime
 with open("data/historical_6hours.json","r") as read_file:
         data = json.load(read_file)
 
+DEGREE_SYMBOL = u"\N{DEGREE SIGN}C"
+
+def format_temperature(temp):
+    return f"{temp}{DEGREE_SYMBOL}"
+
 def convert_time(iso_string):
     d = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S%z")
     return d.strftime("%I:%M%p")
@@ -11,6 +16,8 @@ def convert_time(iso_string):
 total_precip = 0
 hours_rained = 0
 temp = []
+uv_index = []
+daylight_hours = 0
 for forecast in data:
     # Storing temperatures
     temp.append([forecast["Temperature"]["Metric"]["Value"],convert_time(forecast["LocalObservationDateTime"])])
@@ -22,9 +29,38 @@ for forecast in data:
     else:
         hours_rained = hours_rained
     # Number of daylight hours recorded
-    
+    if forecast["IsDayTime"] == True:
+        daylight_hours = daylight_hours + 1
+    else:
+        daylight_hours = daylight_hours
+    # Store UV Index
+    uv_index.append([forecast["UVIndex"],convert_time(forecast["LocalObservationDateTime"])])
 temp.sort()
-print(temp)
+uv_index.sort()
 
-# with open("Weather_Summary.txt","w") as file:
-    # file.write(f'x')
+max_uv = uv_index[-1]
+max_uv_times = []
+for x in uv_index:
+    if x[0] >= max_uv[0]:
+        max_uv.append(x)
+        max_uv_times.append(x[1])
+max_uv_times = ', '.join(max_uv_times)
+
+max_temp = temp[-1]
+max_temp_times = []
+for x in temp:
+    if x[0] >= max_temp[0]:
+        max_temp.append(x)
+        max_temp_times.append(x[1])
+max_temp_times = ', '.join(max_temp_times)
+
+min_temp = temp[0]
+min_temp_times = []
+for x in temp:
+    if x[0] <= min_temp[0]:
+        min_temp.append(x)
+        min_temp_times.append(x[1])
+min_temp_times = ', '.join(min_temp_times)
+
+with open("Weather_Summary.txt","w") as file:
+    file.write(f"The minimum temperature in this dataset is {format_temperature(min_temp[0])}, which occurs at {min_temp_times}. The maximum temperature in this dataset is {format_temperature(max_temp[0])}, which occurs at {max_temp_times}. {total_precip}mm of precipitation fell over a period of {hours_rained} hours during this dataset. In this dataset there were {daylight_hours} hours of daylight with a maximum UV index of {max_uv[0]} occuring at {max_uv_times}.")
